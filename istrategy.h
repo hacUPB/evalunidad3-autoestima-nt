998b5ace0873da23771f3d82e09dc0f1beb7dc1f
danielposadaa <92950477+danielposadaa@users.noreply.github.com>
2022-10-17 19:14:24 -0500

    Update istrategy.h

--- istrategy.h
+++ istrategy.h
@@ -1,1 +1,16 @@
+#ifndef ISTRATEGY_H_
+#define ISTRATEGY_H_
 
+typedef void (*Algoritmo)(void *);
+
+typedef struct
+{
+    Algoritmo Algoritmo1;
+    
+} istrategy ; 
+
+istrategy *istrategy_new();
+void istrategy_ctor(istrategy*);
+void istrategy_dtor(istrategy*);
+char* Fcion(istrategy*);
+#endif
