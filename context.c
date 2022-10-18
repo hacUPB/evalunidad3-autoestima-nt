1dd721c5702a6f10721034e3bb8e83e618d4f720
juanda22x <83099249+juanda22x@users.noreply.github.com>
2022-10-17 20:55:51 -0500

    Update context.c

--- context.c
+++ context.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "context.h"
 
+context *context_new()
+{   
+    return(context*)malloc(sizeof(context));
+}
+
+void context_ctor(context* this, void* istrategy)
+{
+    this->istrategy = (istrategy*)istrategy;
+
+    this->nombre = malloc(sizeof(char[20]));
+
+    strcpy(this->nombre,"");
+}
+
+void context_dtor(context*)
+{
+   free(this->nombre); 
+}
+
+void SetStrategy(context* this, void* istrategy)
+{
+    this->istrategy = (istrategy*)istrategy;
+}
+
+void DoSomeBusinessLogic(context* this)
+{
+    this->istrategy->Algoritmo1(this->nombre);
+}
