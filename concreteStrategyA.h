abd3d522f26a942b89aeaa6692eb9eda055649cf
juanda22x <83099249+juanda22x@users.noreply.github.com>
2022-10-17 20:45:22 -0500

    Update concreteStrategyA.h

--- concreteStrategyA.h
+++ concreteStrategyA.h
@@ -1,1 +1,14 @@
+#ifndef CONCRETESTRATEGYA_H
+#define CONCRETESTRATEGYA_H
 
+#include "istrategy.h"
+
+typedef struct 
+{
+    istrategy strategyA;
+
+} concreteStrategyA; 
+
+concreteStrategyA *new_concreteStrategyA(); 
+void concreteStrategyA_ctor(concreteStrategyA*);
+void concreteStrategyA_dtor(concreteStrategyA*);
