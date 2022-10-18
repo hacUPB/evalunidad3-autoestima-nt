e345d225a1eff18f4b071ff7b57eb498d7b9c7f2
danielposadaa <92950477+danielposadaa@users.noreply.github.com>
2022-10-17 19:14:44 -0500

    Update istrategy.c

--- istrategy.c
+++ istrategy.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "istrategy.h"
 
+void __Algoritmo_void(void *nombre)
+{
+    char * resultado = malloc(sizeof(char[20]));
+    strcpy(resultado,"No hay strategy");
+    printf("%s", resultado);
+    free(resultado);
+
+}
+
+istrategy *istrategy_new()
+{
+    return (istrategy*)malloc(sizeof(istrategy));
+}
+
+void istrategy_ctor(istrategy* this)
+{
+    this->Algoritmo1 = &__Algoritmo_void;
+}
+
+void istrategy_dtor(istrategy* this)
+{
+    free(this);
+}
