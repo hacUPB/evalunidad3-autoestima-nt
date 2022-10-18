7f88b31d514d309d743ea20016a8b4ca8b85a4a7
danielposadaa <92950477+danielposadaa@users.noreply.github.com>
2022-10-18 09:25:01 -0500

    Update concreteStrategyB.h

--- concreteStrategyB.h
+++ concreteStrategyB.h
@@ -1,14 +1,1 @@
-#ifndef CONCRETESTRATEGYB_H
-#define CONCRETESTRATEGYB_H
-#include "istrategy.h"
-
-typedef struct 
-{
-    istrategy istrategy_B;
-
-} concreteStrategyB; 
-
-concreteStrategyB *concreteStrategyB_new(); 
-void concreteStrategyB_ctor(concreteStrategyB*);
-void concreteStrategyB_dtor(concreteStrategyB*);
 
