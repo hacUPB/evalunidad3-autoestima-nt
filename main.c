0d17fe97357c833b66e42b564e0c76a78a3d1c22
danielposadaa <92950477+danielposadaa@users.noreply.github.com>
2022-10-17 19:11:21 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "context.h"
+#include "concreteStrategyA.h"
+#include "concreteStrategyB.h"
+#include "istrategy.h"
+
+int main(void)
+{
+
+istrategy *istrategy = istrategy_new();
+istrategy_ctor(istrategy);
+
+concreteStrategyA *concreteStrategyA = concreteStrategyA_new();
+concreteStrategyA_ctor(concreteStrategyA);
+
+concreteStrategyB *concreteStrategyB = concreteStrategyB_new();
+concreteStrategyB_ctor(concreteStrategyB);
+
+context *context = context_new();
+context_ctor(context);
+
+printf("Client: Strategy is set to normal sorting.");
+SetStrategy(context,concreteStrategyA);
+DoSomeBusinessLogic(context);
+printf("\n");
+printf("Client: Strategy is set to normal sorting.");
+SetStrategy(context,concreteStrategyB);
+DoSomeBusinessLogic(context);
+
+context_dtor(context);
+free(context);
+
+istrategy_dtor(istrategy);
+free(istrategy);
+
+concreteStrategyA_dtor(concreteStrategyA);
+free(concreteStrategyA);
+
+concreteStrategyB_dtor(concreteStrategyB);
+free(concreteStrategyB);
+
+return 0;
+
+}
