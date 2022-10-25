5611340d526b6f9faae579f9d97d5c73bea77022
danielposadaa <92950477+danielposadaa@users.noreply.github.com>
2022-10-25 16:14:59 -0500

    Update context.c

--- context.c
+++ context.c
@@ -7,27 +7,20 @@ context *context_New()
 {
     return(context*)malloc(sizeof(context));
 }
-
 void context_ctor(context *this, void *interface)
 {
     this->interface = (istrategy*)interface;
-
     this->nombre = malloc(sizeof(char[20]));
-
     strcpy(this->nombre,"a,b,c,d,e");
 }
-
 void context_dtor(context *this)
 {
    free(this->nombre);
 }
-
 void SetStrategy(context *this, void *interface)
 {
     this->interface = (istrategy*)interface;
-
 }
-
 void DoSomeBusinessLogic(context *this)
 {
     this->interface->Algoritmo_1(this->nombre);
