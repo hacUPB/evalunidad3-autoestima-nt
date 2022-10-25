6b911e69e9842c92ea694edeb7314463d14ef159
Davidkraai23 <84156615+NoName588@users.noreply.github.com>
2022-10-24 19:47:51 -0500

    szs

--- concreteStrategyB.c
+++ concreteStrategyB.c
@@ -1,1 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "concreteStrategyB.h"
+#include "Context.h"
 
+concreteStrategyB_ctor *nuevaestrategiaB()
+{
+    return (concreteStrategyB*)malloc(sizeof(concreteStrategyB));
+}
+
+void algortimB(void * nombre)
+{
+
+    printf("Contexto: Imprimimos los datos pero invertidos:");
+
+
+    char* lista = malloc(sizeof(char[15]));
+
+
+    strcpy(lista,(char*)nombre);
+    Reverse(lista);
+
+    free(lista);
+
+}
+
+void Reverse (char * lista)
+{
+    char *ptr1, *ptr2;
+    ptr1 = lista;
+    ptr2 = lista + strlen(lista) - 1;
+
+    char NInver = *ptr2;
+    do
+    {
+
+        if (NInver != '\0')
+        {
+
+            ptr2--;
+
+            printf("%c", NInver);
+
+            NInver = *ptr2;
+        }
+        else
+        {
+
+            ptr2--;
+        }
+
+    } while (ptr2 >= ptr1);
+    printf("\n");
+}
+
+void ctorEstrategiaB(estrategiaB *this)
+{
+
+    ctorInterfaz((Interfaz*)this);
+
+
+}
+
+void dtorEstrategiaB(estrategiaB *this)
+{
+    dtorInterfaz((Interfaz*)this);
+}
