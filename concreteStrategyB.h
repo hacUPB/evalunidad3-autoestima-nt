49d6117df612ceb2f99bbf75e74e346351ad0844
juanda22x <83099249+juanda22x@users.noreply.github.com>
2022-10-25 16:47:06 -0500

    fin

--- concreteStrategyB.h
+++ concreteStrategyB.h
@@ -11,5 +11,6 @@ typedef struct
 concreteStrategyB *new_concreteStrategyB(); 
 void concreteStrategyB_ctor(concreteStrategyB*);
 void concreteStrategyB_dtor(concreteStrategyB*);
+void invert (char*);
 
 #endif
