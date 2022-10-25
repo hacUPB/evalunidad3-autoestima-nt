7909fe11213df11b2b8c2a259e4369c5068af3a0
juanda22x <83099249+juanda22x@users.noreply.github.com>
2022-10-25 16:47:50 -0500

    fin

--- concreteStrategyB.c
+++ concreteStrategyB.c
@@ -4,30 +4,40 @@
 #include "concreteStrategyB.h"
 #include "context.h"
 
-concreteStrategyB *nuevaestrategiaB()
+concreteStrategyB *new_concreteStrategyB()
 {
     return (concreteStrategyB*)malloc(sizeof(concreteStrategyB));
 }
-void Algoritmo1B(void * name)
+
+void Algoritmo1B(void * nombre)
 {
-    printf("Client: Strategy is set to reverse sorting.");
-    char* list = malloc(sizeof(char[20]));
-    strcpy(list,(char*)name);
-    Reverse(list);
-    free(list);
+    printf("Contexto: Imprimimos los datos pero invertidos:");
+
+
+    char* lista = malloc(sizeof(char[15]));
+
+
+    strcpy(lista,(char*)nombre);
+    invert(lista);
+
+    free(lista);
 }
-void invert (char * list)
+
+void invert (char * lista)
 {
     char *puntero_1, *puntero_2;
-    puntero_1 = list;
-    puntero_2 = list + strlen(list) - 1;
+    puntero_1 = lista;
+    puntero_2 = lista + strlen(lista) - 1;
+
     char noInvert = *puntero_2;
     do
     {
         if (noInvert != '\0')
         {
             puntero_2--;
+
             printf("%c", noInvert);
+
             noInvert = *puntero_2;
         }
         else
@@ -37,11 +47,13 @@ void invert (char * list)
     } while (puntero_2 >= puntero_1);
     printf("\n");
 }
+
 void concreteStrategyB_ctor(concreteStrategyB *this)
 {
     istrategy_ctor((istrategy*)this);
     this->strategyB.Algoritmo_1 = Algoritmo1B;
 }
+
 void concreteStrategyB_dtor(concreteStrategyB *this)
 {
     istrategy_dtor((istrategy*)this);
